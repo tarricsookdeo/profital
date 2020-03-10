dc41763d01659ef7a9aad41ef239dbb1d1cc4b87
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-03-10 18:33:31 -0400

    Will add nested routes.

--- profital-api/config/routes.rb
+++ profital-api/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :companies
   resources :trades
+
+  resources :companies, only: [:index, :show] do
+    resources :trades, only: [:index, :show]
+  end
 end
