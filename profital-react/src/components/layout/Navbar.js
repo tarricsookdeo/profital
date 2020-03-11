f7d893db5416d72ab551de244276d73585e12f40
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-03-11 17:34:24 -0400

    Will create a basic navbar.

---
+++ profital-react/src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul>
+        <li>Portfolio</li>
+        <li>Trades</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
