4c4f16c72401f140989d94bfa920471dd92f6781
github username <github email address>
2020-03-15 04:47:39 -0400

    Will add seed data.

--- profital-api/db/seeds.rb
+++ profital-api/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Companies
+companies = Company.create(
+    [
+        { ticker: 'AAPL', position_id: 1 },
+    ]
+)
+
+# Create Positions
+positions = Position.create(
+    [
+        { company_id: 1, shares: 20, avg_price: 205.63, total_cost: 4112.60 }
+    ]
+)
+
+# Create Trades
+trades = Trade.create(
+    [
+        { position_id: 1, shares: 10, avg_price: 200.50, date: Date.parse('01/01/2020') },
+        { position_id: 1, shares: 10, avg_price: 210.75, date: Date.parse('01/02/2020') }
+    ]
+)
