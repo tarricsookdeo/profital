9aad7240d5f9fc5847e87f3fd6261dc3afc14d42
github username <github email address>
2020-03-15 06:07:45 -0400

    Will add nested routes.

--- profital-api/config/routes.rb
+++ profital-api/config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   resources :positions
   resources :companies
   resources :trades
+
+  resources :companies, only: [:index, :show] do
+    resources :positions, only: [:index, :show]
+  end
+
+  resources :companies do
+    resources :positions do
+      resources :trades
+    end
+  end
 end
