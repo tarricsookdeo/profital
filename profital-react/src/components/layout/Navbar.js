763b244c7b80f68280742509f85672b07b15a99e
github username <github email address>
2020-03-16 01:18:45 -0400

    Will add new navbar.

--- profital-react/src/components/layout/Navbar.js
+++ profital-react/src/components/layout/Navbar.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <h3>Profital</h3>
-      <ul>
-        <Link to='/'>
-          <li>Watchlist</li>
-        </Link>
-      </ul>
+    <nav className='bg-blue-700 h-12'>
+      <div className='flex flex-row'>
+        <h1 className='text-white font-bold tracking-widest text-xl mt-2 ml-20'>
+          PROFITAL
+        </h1>
+        <button className='text-white mt-2 ml-auto mr-20 hover:text-purple-500'>
+          My Watchlist
+        </button>
+      </div>
     </nav>
   );
 }
